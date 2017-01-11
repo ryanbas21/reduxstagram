f11f17108df51182ec6d298d97e6388c96d83808
ryanbas21 <ryanbas21@gmail.com>
2017-01-11 09:28:16 -0800

    saving

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router'
+import CSSTransitionGroup from 'react-addons-css-transition-group'
 const Photo = (props) =>
         <figure className='grid-figure'>
           <div className='grid-photo-wrap'>
            <Link to={`/view/${props.post.code}`}>
-            {props.post.caption}
+            <img src={props.post.display_src} alt={props.post.caption} className='grid-photo'/>
           </Link>
+          <CSSTransitionGroup transitionName='like' transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+                  <span key={props.post.likes} className="likes-heart">{props.post.likes}</span>
+          </CSSTransitionGroup>
          </div>
+         <figcaption>
+                 <p>{props.post.caption}</p>
+                 <div className='control-buttons'>
+                  <button className='likes'>&hearts; {props.post.likes}</button>
+                  <Link className='button' to={`/view/{props.post.code}`}>
+                   <span className='comment-count'>
+                        <span className='speech-buble'>
+                        {props.comments[props.post.code] ? props.comments[props.post.code].length : 0}
+                        </span>
+                   </span>
+                  </Link>
+                 </div>
+         </figcaption>
         </figure>
 
 export default Photo;
