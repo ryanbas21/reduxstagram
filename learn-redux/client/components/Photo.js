08e8e84be4ac1ab1ecfd36e19c1c760b4b238889
ryanbas21 <ryanbas21@gmail.com>
2017-01-11 09:12:09 -0800

    saving updates and working mvp

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import { Link } from 'react-router'
 const Photo = (props) =>
         <figure className='grid-figure'>
-          I am a photo
+          <div className='grid-photo-wrap'>
+           <Link to={`/view/${props.post.code}`}>
+            {props.post.caption}
+          </Link>
+         </div>
         </figure>
 
 export default Photo;
--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import { render } from 'react-dom'
+import { Link } from 'react-router';
 import Photo from './Photo';
 class PhotoGrid extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
     <div className='photo-grid'>
+
       {this.props.posts.map( (post,index) => <Photo { ...this.props}  key={index+post} i={index} post={post} />)}
     </div>
    )
