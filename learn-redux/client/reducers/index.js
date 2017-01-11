d440ce4e44da8213587f08aaa72af4a282d11602
ryanbas21 <ryanbas21@gmail.com>
2017-01-11 13:29:32 -0800

    save

--- learn-redux/client/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux'
-import comments from './comments'
-import posts from './posts'
-
-
-const rootReducer = combineReducers({posts,comments, routing:routerReducer })
-
-export default rootReducer
