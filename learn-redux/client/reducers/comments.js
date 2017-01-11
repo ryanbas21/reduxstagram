e369b04bda6742e272a965010a89fc6eec5a06ca
ryanbas21 <ryanbas21@gmail.com>
2017-01-11 06:54:34 -0800

    saving

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,6 @@
+function Comments (state = [], action) {
+
+  return state;
+}
+
+export default Comments
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -5,14 +5,17 @@ import Main from './components/Main'
 import PhotoGrid from './components/PhotoGrid'
 import Single from './components/Single'
 import { Router, Route, browserHistory, IndexRoute, } from 'react-router'
-
+import { Provider } from 'redux';
+import store, { history } from './store'
 const router =  (
+  <Provider store={store} >
   <Router history={browserHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={PhotoGrid} />
       <Route path='/view/:postid' component={Single} />
     </Route>
   </Router>
+</Provider>
 )
 
 
